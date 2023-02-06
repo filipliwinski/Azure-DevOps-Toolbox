d2deb9f470e98c330f0158908deb02f6f28a1c79
Filip Liwiński <filipliwinski@users.noreply.github.com>
2023-02-06 23:50:18 +0100

    Updated readme with how to run instruction

--- src/demo.ps1
+++ src/demo.ps1
@@ -1,6 +1,6 @@
 # MIT License
 
-# Copyright (c) 2021 Filip Liwiński
+# Copyright (c) 2021-2023 Filip Liwiński
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -36,8 +36,6 @@ $patToken = '<put the Personal Access Token here>'
 . .\ReleaseDefinitions.ps1
 . .\git\Repositories.ps1
 . .\git\PolicyConfigurations.ps1
-
-# $apiClient = [AzureDevOpsServicesAPIClient]::new($organizationName, $serviceHost, $patToken)
 
 $VerbosePreference = "Continue"   # Show verbose logs
 
