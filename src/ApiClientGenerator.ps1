b862b967bd2c0da129d7e8d339e685fd7a944df2
Filip Liwiński <9355103+filipliwinski@users.noreply.github.com>
2023-04-04 23:49:45 +0200

    Introduced ApiClientGenerator (#4)
    * Introduced ApiClientGenerator
    
    * fix for request path params
    
    * Added AzureDevOpsApiClient, fixed params
    
    * Fixed issue with generating endpoints for multiple methods
    
    * Generate scripts to include ApiClients modules
    
    * updated readme
    
    * Omitted query params
    
    * Organization and projectName moved to base client
    
    * handle duplicates of functions
    
    * Target project controlled by useTargetProject param
    
    * PolicyConfigurations, Refs and Repositories updated
    
    * Updated BuildDefinitions
    
    * Updated readme
    
    * Updated readme with how to run instruction
    
    * Updated demo and existing scripts
    
    * Updated license information
    
    * Updated license information
    
    * Removed AzureDevOpsServicesAPIClient
    
    * Added comments
    
    * ApiClients moved to auto-generated/
    
    * Fixed imports
    
    * Updates to readme
    
    * Additional updates
    
    * Use proper clients in scripts
    
    ---------
    
    Co-authored-by: Filip Liwiński <filipliwinski@users.noreply.github.com>

---
+++ src/ApiClientGenerator.ps1
@@ -0,0 +1,240 @@
+# Copyright (c) Filip Liwiński
+# Licensed under the MIT License. See the LICENSE file in the project root for license information.
+
+$remoteRepoLocation = "https://github.com/MicrosoftDocs/vsts-rest-api-specs.git"
+
+# The API specification is stored in this location to avoid too long file names
+$localRepoLocation = "c:/temp/vsts-rest-api-specs"
+
+# Define the name of the variable used for selecting the target project
+$useTargetProjectVariable = '$useTargetProject'
+
+# Location of auto-generated API Clients
+$apiClientsDirectory = '.\..\auto_generated\ApiClients'
+
+function ComputeFunctionHash {
+    param (
+        [String] $function
+    )
+
+    $stringAsStream = [System.IO.MemoryStream]::new()
+    $writer = [System.IO.StreamWriter]::new($stringAsStream)
+    $writer.write($function)
+    $writer.Flush()
+    $stringAsStream.Position = 0
+    return Get-FileHash -InputStream $stringAsStream -Algorithm MD5 | Select-Object Hash
+}
+
+function ExtractMethods {
+    param (
+        [Object] $object,
+        [String] $path
+    )
+
+    $methods = $object.psobject.properties.name
+    $objectValue = $object.psobject.properties.value
+    $functions = @{}
+
+    if ($objectValue.GetType() -eq [System.Management.Automation.PSCustomObject]) {
+        # System.Management.Automation.PSCustomObject
+        $function = CreatePsFunction -object $objectValue -path $path -method $methods
+        $functionHash = ComputeFunctionHash -function $function
+        $functions.Add($functionHash, $function)
+    }
+    else {
+        # System.Object[]
+        $methodsArray = $methods.Split(' ')
+        for ($i = 0; $i -lt $methodsArray.Count; $i++) {
+            # System.Management.Automation.PSCustomObject
+            $function = CreatePsFunction -object $objectValue[$i] -path $path -method $methodsArray[$i]
+            $functionHash = ComputeFunctionHash -function $function
+            $functions.Add($functionHash, $function)
+        }
+    }
+
+    return $functions.Values
+}
+
+function CreatePsFunction {
+    param (
+        [Object] $object,
+        [String] $path,
+        [string] $method
+    )
+
+    # List parameters
+    $parameters = $object.parameters
+
+    $sbParam = [System.Text.StringBuilder]::new()
+    $body = '$null'
+    foreach ($parameter in $parameters) {
+        if($null -ne $parameter.in -and
+            ($parameter.in -eq 'path' -or $parameter.in -eq 'body') -and
+            $parameter.name -ne 'collection' -and
+            $parameter.name -ne 'organization' -and
+            $parameter.name -ne 'project') {
+            $parameterName = $parameter.name.Replace('$','')
+            $parameterType = $parameter.type ?? 'PSObject'
+            if ($parameterType -eq 'integer') {
+                $parameterType = 'int'
+            }
+            if ($parameterName.Contains('.')) {
+                $parameterName = $parameterName.Split('.')[1]
+            }
+            [void]$sbParam.Append("[$parameterType] $" + "$($parameterName), ")
+
+            if($parameterName -eq 'body') {
+                $body = '$body'
+            }
+        }
+    }
+
+    $sbParamString = $sbParam.ToString()
+
+    $parametersString = ""
+    if ($sbParamString.Length -gt 2) {
+        $parametersString = $sbParamString.Substring(0,$sbParamString.Length-2)
+    }
+
+    # Include [bool] $useTargetProject parameter
+    $parametersString = "[bool] $useTargetProjectVariable, " + $parametersString
+    $parametersString = $parametersString.TrimEnd(', ')
+
+    # Adjust description and path
+    $description = $object.description -replace '\n', ''
+    $path = $path -replace '\$', ''
+    $path = $path -replace '{', '$' -replace '}', ''
+    $path = $path -replace '\$organization', ''
+    $path = $path -replace '\$collection', ''
+    $path = $path -replace '\$project', ''
+    $path = $path -replace '/_apis', ''
+    $path = $path.TrimStart('/')
+
+    $functionName = $object."x-ms-vss-method"
+
+    # Handle duplicates for Head calls
+    if ($method -eq 'head') {
+        $functionName = $functionName + '_Head'
+    }
+
+    # Generate function
+    $sb = [System.Text.StringBuilder]::new()
+    [void]$sb.AppendLine('    # ' + $description)
+    [void]$sb.AppendLine('    [PSObject] ' + $functionName + '(' + $parametersString + ') {')
+    [void]$sb.AppendLine('        return $this.Request(' + $useTargetProjectVariable + ', ''' + $method + ''', "' + $path + '", $this.apiVersion, ' + $body + ')')
+    [void]$sb.AppendLine('    }')
+
+    return $sb.ToString()
+}
+
+if (!(Test-Path -Path $localRepoLocation)) {
+    Write-Host "Cloning $remoteRepoLocation..."
+    git clone $remoteRepoLocation $localRepoLocation
+}
+
+$specifications = Get-ChildItem -Path "$localRepoLocation\specification" -Directory
+$p_index = 1
+foreach ($specification in $specifications)
+{
+    $progress = [math]::floor($p_index / $specifications.count * 100)
+
+    Write-Progress -Activity "Generating clients..." -Status "$progress% Complete [$($specification.name)]" -PercentComplete $progress
+    $p_index++
+
+    $apiVersions = Get-ChildItem -Path "$localRepoLocation\specification\$($specification.name)" -Directory
+
+    foreach ($apiVersion in $apiVersions)
+    {
+        $endpoints = Get-ChildItem -Path "$localRepoLocation\specification\$($specification.name)\$($apiVersion.name)" -File
+
+        foreach ($endpoint in $endpoints)
+        {
+            $endpointName = $endpoint.name.substring(0,1).toupper()+$endpoint.name.substring(1, $endpoint.name.length-6)
+            $endpointName = $endpointName -replace '-o', 'O'
+            $apiClientName = $endpointName + "ApiClient"
+
+            $jsonSpec = Get-Content -Path "$localRepoLocation\specification\$($specification.name)\$($apiVersion.name)\$($endpoint.name)"
+
+            # Replace empty keys
+            $jsonSpec = $jsonSpec -replace '""', '"empty"'
+
+            $spec = $jsonSpec | ConvertFrom-Json
+
+            $sb = [System.Text.StringBuilder]::new()
+            [void]$sb.AppendLine("# This file was auto-generated. Do not edit.")
+            [void]$sb.AppendLine("")
+            [void]$sb.AppendLine("using module .\..\..\..\src\AzureDevOpsApiClient.psm1")
+            [void]$sb.AppendLine("")
+            [void]$sb.AppendLine("class $apiClientName : AzureDevOpsApiClient {")
+            [void]$sb.AppendLine('    [string] $apiVersion = ''' + $spec.info.version + '''')
+            [void]$sb.AppendLine("")
+            [void]$sb.AppendLine("    # $apiClientName(" + '[string] $serviceHost, [string] $organization, [string] $projectName, [string] $personalAccessToken)')
+            [void]$sb.AppendLine('    #     : base ($serviceHost, $organization, $projectName, $personalAccessToken, $serviceHost, $organization, $projectName, $personalAccessToken) {}')
+            [void]$sb.AppendLine("")
+            [void]$sb.AppendLine("    $apiClientName(" + '[string] $sourceServiceHost, [string] $sourceOrganization, [string] $sourceProjectName, [string] $sourcePersonalAccessToken, [string] $targetServiceHost, [string] $targetOrganization, [string] $targetProjectName, [string] $targetPersonalAccessToken)')
+            [void]$sb.AppendLine('        : base ($sourceServiceHost, $sourceOrganization, $sourceProjectName, $sourcePersonalAccessToken, $targetServiceHost, $targetOrganization, $targetProjectName, $targetPersonalAccessToken) {}')
+            [void]$sb.AppendLine("")
+
+
+            foreach ($path in $spec.paths) {
+                $paths = $path.psobject.properties.name
+                $pathObject = $path.psobject.properties.value
+
+                if ($pathObject.count -gt 0) {  # skip specs with empty paths
+                    if ($pathObject.GetType() -eq [System.Management.Automation.PSCustomObject]) {
+                        # System.Management.Automation.PSCustomObject
+                        $functionsString = ExtractMethods -object $pathObject -path $paths
+                        [void]$sb.AppendLine($functionsString)
+                    }
+                    else {
+                        # System.Object[]
+                        for ($i = 0; $i -lt $pathObject.Count; $i++) {
+                            # System.Management.Automation.PSCustomObject
+                            $pathsArray = $paths.Split(' ')
+                            $functionsString = ExtractMethods -object $pathObject[$i] -path $pathsArray[$i]
+                            [void]$sb.AppendLine($functionsString)
+                        }
+                    }
+                }
+            }
+
+            [void]$sb.AppendLine("}")
+            $apiClientClass = $sb.ToString()
+
+            $apiClientDirectory = "$apiClientsDirectory\$($apiVersion.name)"
+
+            if (!(Test-Path -Path $apiClientDirectory)) {
+                New-Item $apiClientDirectory -ItemType Directory
+            }
+
+            $apiClientClass | Out-File -FilePath "$apiClientDirectory\$apiClientName.psm1"
+        }
+    }
+}
+
+Write-Progress -Activity "Generating clients..." -Completed
+
+# Generate scripts to include ApiClients modules
+if (!(Test-Path -Path $apiClientsDirectory)) {
+    New-Item $apiClientsDirectory -ItemType Directory
+}
+
+$apiClientVersions = Get-ChildItem $apiClientsDirectory -directory
+
+foreach ($version in $apiClientVersions) {
+    $apiClients = Get-ChildItem $version -file
+    $sb = [System.Text.StringBuilder]::new()
+    [void]$sb.AppendLine("# This file was auto-generated. Do not edit.")
+    [void]$sb.AppendLine("")
+
+    foreach ($apiClient in $apiClients) {
+        $relativePath = Get-Item $apiClient | Resolve-Path -Relative
+        $stringToRemove = $apiClientsDirectory.Trim('.', '\')
+        $relativePath = $relativePath.Replace("$stringToRemove\", "").Replace("..", ".")
+        [void]$sb.AppendLine("using module ""$relativePath""")
+    }
+
+    $sb.ToString() | Out-File -FilePath "$apiClientsDirectory\$($version.PSChildName).ps1"
+}
+
+# Remove-Item -Path $localRepoLocation -Recurse -Force
--- src/VariableGroups.ps1
+++ src/VariableGroups.ps1
@@ -1,110 +1,70 @@
-# MIT License
+# Copyright (c) Filip Liwiński
+# Licensed under the MIT License. See the LICENSE file in the project root for license information.
 
-# Copyright (c) 2021 Filip Liwiński
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+$taskAgentApiClient = [TaskAgentOnpremApiClient]::new($tfsServiceHost, $organization, $projectName, $patToken,
+                                    $targetTfsServiceHost, $targetOrganization, $targetProjectName, $targetPatToken)
 
 function Add-VariableGroup {
     param(
-        [string] $projectName,
-        [AzureDevOpsServicesAPIClient] $apiClient,
+        [switch] $useTargetProject,
         [PSObject] $variableGroup
     )
 
-    return $apiClient.AddVariableGroup($projectName, $variableGroup)
+    return $taskAgentApiClient.AddVariableGroup($useTargetProject, $variableGroup)
 }
 
 function Copy-VariableGroup {
     param(
-        [string] $sourceProjectName,
-        [string] $targetProjectName,
-        [AzureDevOpsServicesAPIClient] $sourceApiClient,
-        [AzureDevOpsServicesAPIClient] $targetApiClient,
-        [int] $variableGroupId
+        [switch] $useTargetProject,
+        [int] $id
     )
-    $namePostfix = ""
+    $namePostfix = "- copy"
 
-    $sourceVariableGroup = $sourceApiClient.GetVariableGroupById($sourceProjectName, $variableGroupId)
-
-    if ($sourceApiClient.ServiceHost -eq $targetApiClient.ServiceHost -and 
-        $sourceApiClient.Organization -eq $targetApiClient.Organization -and
-        $sourceProjectName -eq $targetProjectName) {
-        $namePostfix = "- copy"
-    }
+    $variableGroup = $taskAgentApiClient.GetVariableGroup($useTargetProject, $id)
 
     $newVariableGroup = @{
-        "description" = $sourceVariableGroup.description
-        "name" = "$($sourceVariableGroup.name) $namePostfix"
-        "providerData" = $sourceVariableGroup.providerData
-        "type" = $sourceVariableGroup.type
-        "variables" = $sourceVariableGroup.variables
+        "description" = $variableGroup.description
+        "name" = "$($variableGroup.name) $namePostfix"
+        "providerData" = $variableGroup.providerData
+        "type" = $variableGroup.type
+        "variableGroupProjectReferences" = $variableGroup.variableGroupProjectReferences
+        "variables" = $variableGroup.variables
     }
 
-    return $targetApiClient.AddVariableGroup($targetProjectName, $newVariableGroup)
+    return $taskAgentApiClient.AddVariableGroup($useTargetProject, $newVariableGroup)
 }
 
 function Get-VariableGroup {
     param (
-        [string] $projectName,
-        [string] $outputPath = '',
-        [AzureDevOpsServicesAPIClient] $apiClient,
-        [int] $variableGroupId
+        [switch] $useTargetProject,
+        [int] $id
     )
 
-    $variableGroup = $apiClient.GetVariableGroupById($projectName, $variableGroupId)
-    return $variableGroup
-}
-
-function Get-VariableGroupByName {
-    param (
-        [string] $projectName,
-        [AzureDevOpsServicesAPIClient] $apiClient,
-        [string] $variableGroupName
-    )
-
-    $variableGroup = $apiClient.GetVariableGroupByName($projectName, $variableGroupName)
+    $variableGroup = $taskAgentApiClient.GetVariableGroup($useTargetProject, $id)
     return $variableGroup
 }
 
 function Get-VariableGroups {
     param (
-        [string] $projectName,
-        [AzureDevOpsServicesAPIClient] $apiClient
+        [switch] $useTargetProject
     )
 
-    $variableGroups = $apiClient.GetVariableGroups($projectName)
+    $variableGroups = $taskAgentApiClient.GetVariableGroupsById($useTargetProject)
     return $variableGroups
 }
 
 function Export-VariableGroup {
     param (
-        [string] $projectName,
+        [switch] $useTargetProject,
         [string] $outputPath = '',
-        [AzureDevOpsServicesAPIClient] $apiClient,
-        [int] $variableGroupId
+        [int] $id
     )
 
     if ($null -eq $outputPath -or '' -eq $outputPath) {
         $outputPath = "."
     }
 
-    $variableGroup = $apiClient.GetVariableGroupById($projectName, $variableGroupId)
+    $variableGroup = $taskAgentApiClient.GetVariableGroup($useTargetProject, $id)
 
     if ($null -ne $variableGroup) {
         New-Item -ItemType Directory -Force -Path $outputPath | Out-Null
@@ -116,16 +76,15 @@ function Export-VariableGroup {
 
 function Export-VariableGroups {
     param (
-        [string] $projectName,
-        [string] $outputPath = '',
-        [AzureDevOpsServicesAPIClient] $apiClient
+        [switch] $useTargetProject,
+        [string] $outputPath = ''
     )
 
     if ($null -eq $outputPath -or '' -eq $outputPath) {
         $outputPath = "."
     }
 
-    $variableGroups = $apiClient.GetVariableGroups($projectName)
+    $variableGroups = $taskAgentApiClient.GetVariableGroups($useTargetProject)
 
     if ($null -ne $variableGroups) {
         New-Item -ItemType Directory -Force -Path $outputPath | Out-Null
